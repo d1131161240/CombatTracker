d678469be7ca66242e90c03bcce7a8ebc8b40f0f
dkq <d1131161240@qq.com>
2022-02-07 23:37:42 +0800

    text modify

--- CombatTracker/CombatTracker.lua
+++ CombatTracker/CombatTracker.lua
@@ -2,7 +2,7 @@
 -- date: 20220207
 
 
--- Set up some local varialbes to track time and damage
+-- Set up some local var to track time and damage
 local start_time = 0
 local end_time = 0
 local total_time = 0
@@ -17,24 +17,27 @@ function CombatTracker_OnLoad( frame )
     frame:RegisterForDrag("LeftButton")
 end
 
-function CombatTracker_OnEvent( frame, event, ... )
+function CombatTracker_OnEvent(frame, event, ... )
     if event == "PLAYER_REGEN_ENABLE" then
         --This event is called when the player exits combat
+        --这个事件意味着已经脱战了，此时可以读取结束时间
         end_time = GetTime()
         total_time = end_time - start_time
         average_dps = total_damage / total_time
         CombatTracker_UpdateText()
     elseif event == "PLAYER_REGEN_DISABLE" then
         -- This event is called when we enter combat
+        -- 这个事件意味着进入战斗
         -- reset the damage total and start the timer
+        -- 重置数据，并且开始计时和计算伤害
         CombatTrackerFrameText:SetText("In Combat")
         total_damage = 0
         start_time = GetTime()
     elseif event == "UNIT_COMBAT" then
         if not InCombatLockdown() then
-            -- we are not in combat, so ignaor the event
+            -- we are not in combat, so ignore the event
         else
-            local unit, action, modifier, damage, damagetype = ...
+            local unit, action, modifier, damage, damageType = ...
             if unit == "player" and action ~= "HEAL" then
                 total_damage = total_damage + damage
                 end_time = GetTime()
@@ -52,8 +55,8 @@ function CombatTracker_UpdateText()
 end
 
 function CombatTracker_ReportDPS()
-    local msgformat = "%d seconds spent in combat with %d incoming damage. Average incoming DPS was %.2f"
-    local msg = string.format(msgformat, total_time, total_damage, average_dps)
+    local msgFormat = "%d seconds spent in combat with %d incoming damage. Average incoming DPS was %.2f"
+    local msg = string.format(msgFormat, total_time, total_damage, average_dps)
     if GetNumPartyMembers() > 0 then
         SendChatMessage(msg, "PARTY")
     else
